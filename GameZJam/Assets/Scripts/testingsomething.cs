57acfc74014e24cade753fcef66fe521d7575f73
Xistydev <137962387+Xistydev@users.noreply.github.com>
2023-07-14 01:43:53 +0300

    some updates

--- GameZJam/Assets/Scripts/testingsomething.cs
+++ GameZJam/Assets/Scripts/testingsomething.cs
@@ -7,7 +7,7 @@ public class testingsomething : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("ijij");
+        
     }
 
     // Update is called once per frame
