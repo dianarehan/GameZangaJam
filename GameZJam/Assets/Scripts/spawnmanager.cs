e38e8a2b034278ad433c99d20ac29c7ff2297ddc
Xistydev <137962387+Xistydev@users.noreply.github.com>
2023-07-14 10:46:26 +0300

    Update spawnmanager.cs

--- GameZJam/Assets/Scripts/spawnmanager.cs
+++ GameZJam/Assets/Scripts/spawnmanager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class spawnmanager : MonoBehaviour
 {
+    [SerializeField] float time;
+    [SerializeField] float repeatrate;
+    [SerializeField] GameObject moving_platformprefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("spawnobject", time, repeatrate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void spawnobject()
+    {
+        Instantiate(moving_platformprefab, new Vector2(14.63f, Random.Range(0.46f, -1.64f)), transform.rotation);
+    }
+
 }
+    
