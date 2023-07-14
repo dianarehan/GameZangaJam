3c081633d2e6efb453e2c20e2c4a858149c1681a
Xistydev <137962387+Xistydev@users.noreply.github.com>
2023-07-14 09:49:55 +0300

    Update gravityabillity.cs
    changing gravity abililty done

--- GameZJam/Assets/Scripts/gravityabillity.cs
+++ GameZJam/Assets/Scripts/gravityabillity.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class gravityabillity : MonoBehaviour
 {
+    
+    [SerializeField] float gravity;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.gravityScale = gravity;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gravity = -1;
+        }
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            gravity = 1;
+        }
     }
 }
