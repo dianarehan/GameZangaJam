925d1af671654144aadd0e1297351a2fd428d341
Diana Rehan <daiana.rehan.dr@gmail.com>
2023-07-15 03:12:25 +0300

    aaah t3ebt

--- GameZJam/Assets/Scripts/spawnmanager.cs
+++ GameZJam/Assets/Scripts/spawnmanager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -43,4 +43,66 @@ public class spawnmanager : MonoBehaviour
         Instantiate(applesprefab, new Vector2(14.63f, randomxpos), transform.rotation);
     }
 }
-    
+    */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour
+{
+    [SerializeField] float time;
+    [SerializeField] float repeatrate;
+    [SerializeField] GameObject moving_platformprefab;
+    [SerializeField] GameObject projectilesprefab;
+    [SerializeField] GameObject applesprefab;
+    [SerializeField] int platformCountForApple = 5; // Number of platforms to spawn between each apple
+
+    private int platformCount = 0; // Counter for spawned platforms
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnplatforms", time, repeatrate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /* void spawnplatforms()
+     {
+         float xposlimit = 3.44f;
+         float xposlimitt = -3.26f;
+         float randomxpos = Random.Range(xposlimit, xposlimitt);
+         Instantiate(moving_platformprefab, new Vector2(14.63f, randomxpos), Quaternion.identity);
+
+         platformCount++; // Increase the platform counter
+
+         // Check if the platform count reaches the desired number for spawning an apple
+         if (platformCount % platformCountForApple == 0)
+         {
+             // Spawn an apple on the platform
+             Instantiate(applesprefab, new Vector2(14.63f, randomxpos + 1.0f), Quaternion.identity);
+         }
+     }*/
+    void spawnplatforms()
+    {
+        float xposlimit = 3.44f;
+        float xposlimitt = -3.26f;
+        float randomxpos = Random.Range(xposlimit, xposlimitt);
+        GameObject platform = Instantiate(moving_platformprefab, new Vector2(14.63f, randomxpos), Quaternion.identity);
+
+        platformCount++; // Increase the platform counter
+
+        // Check if the platform count reaches the desired number for spawning an apple
+        if (platformCount % platformCountForApple == 0)
+        {
+            // Spawn an apple on the platform
+            GameObject apple = Instantiate(applesprefab, platform.transform.position + new Vector3(0f, 1.0f, 0f), Quaternion.identity);
+            apple.transform.SetParent(platform.transform);
+        }
+    }
+
+}
